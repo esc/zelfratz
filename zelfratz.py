f3474c972e28616a9aa1014295063fb7ffae2724
esc <emergencyselfconstruct@googlemail.com>
2009-03-03 22:43:30 +0100

    basic parsing of track xml implemented

--- zelfratz.py
+++ zelfratz.py
@@ -20,22 +20,18 @@ class release():
         self.label = label
         self.url = url
     def pretty_print(self):
-        print self.name.encode()
+        print "release: ", self.name.encode()
         print "by: ", [a.encode() for a in self.artists]
         print "on label: ", self.label.encode()
         print "url: ", self.url.encode()
 
 class track():
     """ holds really basic information about a track """
-    def __init__(self, name, release, label,url)
+    def __init__(self, name,url):
         self.name = name
-        self.release = release
-        self.label = label
         self.url = url
     def pretty_print(self):
-        print "track: "self.name.encode()
-        print "in release: " self.release.encode()
-        print "on label: ", self.label.encode()
+        print "track: ", self.name.encode()
         print "url: ", self.url.encode()
 
 def __init__():
@@ -52,7 +48,7 @@ def create_api_request(type,search):
         url += "tracks/by_artist/"
     elif type == LABEL:
         url += "releases/by_label/"
-    elif 
+    else:
         print "create_api_request invoked with incorrect argument:" + TYPE
         sys.exit()
     url += search + "?key=" + key
@@ -74,6 +70,18 @@ def parse_release_xml(release_xml):
         label = tmp.firstChild.data
         releases.append(release(name,artists,label,url))
     return releases
+
+def parse_track_xml(track_xml):
+    """ parse xml returned by a 'track' query into a list of track objects """
+    x = xml.dom.minidom.parseString(track_xml)
+    tracks = list()
+    for tra in x.getElementsByTagName('track'):
+        name = tra.getElementsByTagName('name')[0].firstChild.data
+        rel = tra.getElementsByTagName('release')[0]
+        u= rel.getElementsByTagName('url')[0].firstChild.data
+        tracks.append(track(name,u))
+    return tracks
+
     
 def read_key_from_file(filename):
     """ read the application specific key from a file """
@@ -96,8 +104,7 @@ if __name__ ==  "__main__":
     url = create_api_request(ARTIST,"pyro")
     x = do_api_call(url)
     print x
-    #print parse_release_xml(x)
-    #li = parse_release_xml(x)
-    #for l in li:
-    #    l.pretty_print()
+    li = parse_track_xml(x)
+    for l in li:
+        l.pretty_print()
 

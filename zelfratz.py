8072c919e07cef1051ddb04e4798f1bda65957be
esc <emergencyselfconstruct@googlemail.com>
2009-03-03 22:12:37 +0100

    fixed a typo in the module docstring

--- zelfratz.py
+++ zelfratz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #coding=utf-8
 
-""" zelfratz is a tool to track artist and lable relaeses on digital-tunes """
+""" zelfratz is a tool to track artist and lable releases on digital-tunes """
 
 import pycurl
 import xml.dom.minidom

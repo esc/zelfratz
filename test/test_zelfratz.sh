e5d3c93ca260b952eeeb845ec254aec07f991c2f
esc <emergencyselfconstruct@googlemail.com>
2009-03-07 03:19:15 +0100

    testing shell script now only runs unit tests

--- test/test_zelfratz.sh
+++ test/test_zelfratz.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # run zelfratz with test input files
-echo "testing zelfratz now!"
-echo "will now test the command line interface"
-PATH=../:$PATH zelfratz.py -k test_key -a test_artists -l test_labels
+#echo "testing zelfratz now!"
+#echo "will now test the command line interface"
+#PATH=../:$PATH zelfratz.py -k test_key -a test_artists -l test_labels
 echo "will now run unit tests"
 PYTHONPATH=../ python test_zelfratz.py
